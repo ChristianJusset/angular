dd25b1c37fe5dde11ea80ebd5b1f066d15c71cb8
christian jusset <sanchezsaraviachristian@gmail.com>
2021-02-18 18:49:20 -0500

    lazy

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,48 +1,19 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { AdminGuard } from "../guards/admin.guard";
-import { AuthGuard } from "../guards/auth.guard";
-import { AccountSettingsComponent } from "./account-settings/account-settings.component";
-import { BusquedaComponent } from "./busqueda/busqueda.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { Grafica1Component } from "./grafica1/grafica1.component";
-import { HospitalesComponent } from "./mantenimientos/hospitales/hospitales.component";
-import { MedicoComponent } from "./mantenimientos/medicos/medico.component";
-import { MedicosComponent } from "./mantenimientos/medicos/medicos.component";
-import { UsuariosComponent } from "./mantenimientos/usuarios/usuarios.component";
-import { PagesComponent } from "./pages.component";
-import { PerfilComponent } from "./perfil/perfil.component";
-import { ProgressComponent } from "./progress/progress.component";
-import { PromesasComponent } from "./promesas/promesas.component";
-import { RxjsComponent } from "./rxjs/rxjs.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../guards/auth.guard';
+import { PagesComponent } from './pages.component';
+
+
 
 const routes: Routes = [
     { 
         path: 'dashboard', 
         component: PagesComponent,
         canActivate: [ AuthGuard ],
-        children: [
-        { path: '', component: DashboardComponent },
-          
-        { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
-        { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' }},
-        { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica #1' }},
-        { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de cuenta' }},
-        { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'Busquedas' }},
-        { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }},
-        { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' }},
-        { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' }},
-        // Mantenimientos
-        { path: 'usuarios', canActivate: [ AdminGuard ], component: UsuariosComponent, data: { titulo: 'Matenimiento de Usuarios' }},
-       
-        { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Hospitales de aplicación' }},
-        { path: 'medicos', component: MedicosComponent, data: { titulo: 'Matenimiento de Medicos' }},
-        { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Matenimiento de Medicos' }},
-        ]
-      }
-      
-]
-
+        canLoad: [ AuthGuard ],
+        loadChildren: () => import('./child-routes.module').then( m => m.ChildRoutesModule )
+    },
+];
 @NgModule({
     imports: [ RouterModule.forChild(routes) ],
     exports: [ RouterModule ]
